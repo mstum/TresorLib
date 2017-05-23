1fa798ad2455b0ad04c807ef2c093616e10aa502
Michael Stum <git@stum.de>
2017-05-23 00:39:47 -0400

    Added Benchmark Project to improve allocations etc.
    Baseline Values to improve on:
    RyuJitX64 13.6719 Gen0 / 42.41 KB Allocated
    LegacyJitX86 9.7656 Gen0 / 30.51 KB Allocated

---
+++ src/TresorLib.Benchmark/Program.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace TresorLib.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<TresorLibBenchmark>();
+
+            Console.ReadLine();
+        }
+    }
+
+    [MemoryDiagnoser]
+    [RyuJitX64Job]
+    [LegacyJitX86Job]
+    public class TresorLibBenchmark
+    {
+        [Benchmark]
+        public string GetValue()
+        {
+            var service = "twitter";
+            var phrase = "I'm the best 17-year old ever.";
+            var config = TresorConfig.Default;
+            config.PasswordLength = 14;
+            config.Space = TresorConfig.AllowedMode.Forbidden;
+            config.Symbols = TresorConfig.AllowedMode.Forbidden;
+            config.MaxRepetition = 1;
+
+            var password = Tresor.GeneratePassword(service, phrase, config);
+
+            return password;
+        }
+    }
+}
