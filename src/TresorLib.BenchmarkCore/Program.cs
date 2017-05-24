771fee38b8cf0162ec6214a468dfcef0ef358322
Michael Stum <git@stum.de>
2017-05-24 06:50:26 -0400

    Added .net Core Benchmark Project
    Allocation there is 8.37 KB

---
+++ src/TresorLib.BenchmarkCore/Program.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace TresorLib.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<TresorLibBenchmark>();
+
+            Console.ReadLine();
+        }
+    }
+
+    [MemoryDiagnoser]
+    [RyuJitX64Job]
+    [CoreJob]
+    public class TresorLibBenchmark
+    {
+        private readonly static TresorConfig Config;
+
+        static TresorLibBenchmark()
+        {
+            Config = TresorConfig.Default;
+            Config.PasswordLength = 14;
+            Config.Space = TresorConfig.AllowedMode.Forbidden;
+            Config.Symbols = TresorConfig.AllowedMode.Forbidden;
+            Config.MaxRepetition = 1;
+        }
+
+        [Benchmark]
+        public string GetValue()
+        {
+            const string service = "twitter";
+            const string phrase = "I'm the best 17-year old ever.";
+
+            var password = Tresor.GeneratePassword(service, phrase, Config);
+
+            return password;
+        }
+    }
+}
